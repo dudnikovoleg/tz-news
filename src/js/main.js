da17ffb8874f7116bc9018dded5ecf3f2d323c42
Alexander Ponomariov <sanik.2008.2008@gmail.com>
2017-04-20 16:23:57 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -162,11 +162,32 @@ $(document).on('ready', function(){
 
 });
 
+/* ---------------------------------------------- /*
+ *  Forms sending
+ /* ---------------------------------------------- */
 $(function(){
 
     $(document).on("submit","#Form",function(e){
 
-        e.preventDefault();
+        e.preventDefault();       
+
+        var stop = false;
+
+        $(this).find("input").each(function(){
+            
+            if( $(this).val().length == 0 ){
+
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+                $(this).parents("form").find('input,textarea').trigger("blur");  
+                stop = true;              
+                
+            }
+
+        });
+
+        if ( stop ) {
+            return false;
+        }        
 
         var m_method=$(this).attr('method');
         var m_action=$(this).attr('action');
@@ -197,6 +218,23 @@ $(function(){
 
         e.preventDefault();
 
+        var stop = false;
+
+        $(this).find("input").each(function(){
+            
+            if( $(this).val().length == 0 ){
+
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+                $(this).parents("form").find('input,textarea').trigger("blur");  
+                stop = true;              
+                
+            }
+
+        });
+        if ( stop ) {
+            return false;
+        }        
+
         var m_method=$(this).attr('method');
         var m_action=$(this).attr('action');
         var m_data=$(this).serialize();
@@ -219,3 +257,143 @@ $(function(){
     });
 
 });
+
+
+/* ---------------------------------------------- /*
+ *  Forms validation
+ /* ---------------------------------------------- */
+
+$(document).on( 'blur' , '[name="name"] , [name="surname"] , [name="email"] , [name="tel"] , [name="question"] ' , function(){
+
+    var field = $(this).attr("name");
+    var fieldValue = $(this).val();
+    var message, error;
+
+    switch (field) {
+
+        case 'name':
+            message = 'Имя должно быть не меньше 3 символов';
+
+            error = is_not_empty( fieldValue , message , 1 );
+
+            if ( error ) {
+                
+                $(this).siblings(".form-field-error").text(error);
+                $(this).addClass("error");
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+
+            } else {
+                $(this).siblings(".form-field-error").empty();
+                $(this).removeClass("error");
+                $(this).parents("form").find('[type="submit"]').removeClass("error");
+            }
+               
+            break;
+
+
+        case 'surname':
+            message = 'Фамилия должна быть не меньше 3 символов';
+            
+            error = is_not_empty( fieldValue , message , 1 );
+
+            if ( error ) {
+
+                $(this).siblings(".form-field-error").text(error);
+                $(this).addClass("error");
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+
+            } else {
+                $(this).siblings(".form-field-error").empty();
+                $(this).removeClass("error");
+                $(this).parents("form").find('[type="submit"]').removeClass("error");
+            }
+             
+            break;
+
+
+        case 'email': 
+
+            var mailRegExp = /^[-\w.]+@([A-z0-9][-A-z0-9]*\.)+[A-z]{2,4}$/i;           
+
+            if( !mailRegExp.test(fieldValue) ) {
+
+                message = 'E-mаil введен не корректно';
+
+                $(this).siblings(".form-field-error").text(message);
+                $(this).addClass("error");
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+                              
+            } else {
+                $(this).siblings(".form-field-error").empty();
+                $(this).removeClass("error");
+                $(this).parents("form").find('[type="submit"]').removeClass("error");
+            }
+
+            break;
+
+
+        case 'tel':            
+
+            var telRegExp = /[0-9]{10}/i;           
+
+            if( !telRegExp.test(fieldValue) || fieldValue.length != 10 ) {
+
+                message = 'Телефон должен состоять из 10 цифр';
+
+                $(this).siblings(".form-field-error").text(message);
+                $(this).addClass("error");
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+                              
+            } else {
+                $(this).siblings(".form-field-error").empty();
+                $(this).removeClass("error");
+                $(this).parents("form").find('[type="submit"]').removeClass("error");
+            }
+
+            break;
+
+
+        case 'question':
+            message = 'Сообщение должно быть не меньше 3 символов';
+            
+            if ( fieldValue.length < 3 ) {
+            
+                $(this).siblings(".form-field-error").text(message);
+                $(this).addClass("error");
+                $(this).parents("form").find('[type="submit"]').addClass("error");
+
+            } else {
+                $(this).siblings(".form-field-error").empty();
+                $(this).removeClass("error");
+                $(this).parents("form").find('[type="submit"]').removeClass("error");
+            }
+
+            break;
+
+   
+    }
+
+    function is_not_empty ( value , message , forbid = 0 , limit = 3 ) { 
+
+        var Reg62=/^[а-яА-ЯёЁa-zA-Z0-9]+$/i;
+        
+
+        if( !Reg62.test(value) && forbid == 1 ) {
+
+            message = 'Только цифры и буквы';
+
+            return message;              
+        }               
+
+        if ( value.length < limit ) {
+            
+            return message;
+        }
+
+        return false;   
+
+    }
+
+
+});
+
